200eea063185bd52120061658842da5cff69abad
Rohit Kheto <rohit@itobuz.com>
2025-01-02 17:05:33 +0530

    feat: add date & time

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const storedArray = localStorage.getItem('array')
-let todoArr = (storedArray) ? JSON.parse(storedArray) : [];
+const storedArray = localStorage.getItem("array");
+let todoArr = storedArray ? JSON.parse(storedArray) : [];
 
 const todoInput = document.getElementById("todo-input");
 const addBtn = document.getElementById("add-btn");
@@ -11,165 +11,191 @@ const clearBtn = document.getElementById("clear");
 const filterBtns = document.querySelectorAll(".filter-button");
 const alert = document.querySelector(".alert");
 
-const createList = (value, active, index) => {
-    const newTask = document.createElement("div");
-    const taskname = document.createElement("p");
-    const taskBtnContainer = document.createElement("div");
-    const checkBtn = document.createElement("button");
-    const checkIcon = document.createElement("img");
-    const deleteBtn = document.createElement("button");
-    const deleteIcon = document.createElement("img");
-    const editBtn = document.createElement("button");
-    const editIcon = document.createElement("img");
+const createList = ({ value, active, dateTime }, index, count) => {
+  const newTask = document.createElement("div");
+  const taskDateTime = document.createElement("p");
+  const taskContent = document.createElement("div");
+  const taskName = document.createElement("p");
+  const taskBtnContainer = document.createElement("div");
+  const checkBtn = document.createElement("button");
+  const checkIcon = document.createElement("img");
+  const deleteBtn = document.createElement("button");
+  const deleteIcon = document.createElement("img");
+  const editBtn = document.createElement("button");
+  const editIcon = document.createElement("img");
 
-    taskname.textContent = value;
-    taskname.setAttribute("id",`task${index}`)
-    taskname.setAttribute('class','task-name fs-5 mb-0 overflow-scroll');
+  taskName.textContent = value;
+  taskName.setAttribute(
+    "class",
+    "task-name fs-5 mb-0 overflow-scroll rounded-1 px-1"
+  );
+  taskName.setAttribute("onMouseOut", `showList()`);
+  taskName.setAttribute("onkeypress", `updateTask(${index}, ${count}, event)`);
 
-    taskBtnContainer.appendChild(checkBtn);
-    taskBtnContainer.appendChild(editBtn);
-    taskBtnContainer.appendChild(deleteBtn);
+  const currentDateTime = new Date(dateTime);
+  taskDateTime.textContent =
+    currentDateTime.toDateString() + " " + currentDateTime.toLocaleTimeString();
+  taskDateTime.setAttribute("class", "mb-2 text-secondary fst-italic");
 
-    checkIcon.src = "images/checked.png";
-    checkIcon.classList.add("task-icon");
+  checkIcon.src = "images/checked.png";
+  checkIcon.classList.add("task-icon");
 
-    checkBtn.classList.add("border-0");
-    checkBtn.classList.add("checkTask");
-    checkBtn.setAttribute("onClick", `completeTask('${index}')`);
-    checkBtn.appendChild(checkIcon);
+  checkBtn.setAttribute("class", "border-0 checkTask");
+  checkBtn.setAttribute("onClick", `completeTask(${index})`);
+  checkBtn.appendChild(checkIcon);
 
-    deleteIcon.src = "images/trash-bin.png";
-    deleteIcon.classList.add("task-icon");
+  deleteIcon.src = "images/trash-bin.png";
+  deleteIcon.classList.add("task-icon");
 
-    deleteBtn.classList.add("border-0");
-    deleteBtn.setAttribute("onClick", `deleteTask(${index})`);
-    deleteBtn.appendChild(deleteIcon);
+  deleteBtn.classList.add("border-0");
+  deleteBtn.setAttribute("onClick", `deleteTask(${index})`);
+  deleteBtn.appendChild(deleteIcon);
 
-    editIcon.src = "images/edit.png";
-    editIcon.classList.add("task-icon");
+  editIcon.src = "images/edit.png";
+  editIcon.classList.add("task-icon");
 
-    editBtn.classList.add("border-0");
-    editBtn.setAttribute("onClick", `editTask('${index}')`);
-    editBtn.appendChild(editIcon);
+  editBtn.classList.add("border-0");
+  editBtn.setAttribute("onClick", `editTask(${count})`);
+  editBtn.appendChild(editIcon);
 
-    if (!active){
-        taskname.classList.add("text-decoration-line-through");
-        editBtn.setAttribute('disabled' ,'true');
-        editBtn.style.cursor ='not-allowed';
-    }
+  if (!active) {
+    taskName.classList.add("text-decoration-line-through");
+    editBtn.setAttribute("disabled", "true");
+    editBtn.style.cursor = "not-allowed";
+  }
 
-    newTask.append(taskname,taskBtnContainer);
-    newTask.setAttribute('class',"task-container d-flex justify-content-between p-2 m-auto rounded-1 mb-2");
-
-    todoList.appendChild(newTask);
+  taskBtnContainer.append(checkBtn, editBtn, deleteBtn);
+  taskContent.append(taskName, taskBtnContainer);
+  taskContent.setAttribute("class", "d-flex justify-content-between");
+  newTask.append(taskDateTime, taskContent);
+  newTask.setAttribute("class", "p-2 m-auto rounded-1 mb-3 task-container");
+  todoList.appendChild(newTask);
 };
 
 const showList = () => {
-    todoList.innerHTML = "";
-    localStorage.setItem("array", JSON.stringify(todoArr));
+  todoList.innerHTML = "";
+  localStorage.setItem("array", JSON.stringify(todoArr));
 
-    if (allBtn.classList.contains('active')) {
-        todoArr.forEach(({ value, active }, index) => createList(value, active, index));
-    }
+  if (allBtn.classList.contains("active")) {
+    todoArr.forEach((obj, index) => createList(obj, index, index));
+  } else if (completeBtn.classList.contains("active")) {
+    let count = 0;
+    todoArr.forEach((obj, index) => {
+      if (!obj.active) {
+        createList(obj, index, count);
+        count++;
+      }
+    });
+  } else if (activeBtn.classList.contains("active")) {
+    let count = 0;
+    todoArr.forEach((obj, index) => {
+      if (obj.active) {
+        createList(obj, index, count);
+        count++;
+      }
+    });
+  }
+};
 
-    else if (completeBtn.classList.contains('active')) {
-        todoArr.forEach(({ value, active }, index) => {
-            if (!active)
-                createList(value, active, index)
-        });
-    }
+const showAlert = (alertMessage) => {
+  alert.style.display = "block";
+  alert.textContent = alertMessage;
 
-    else if (activeBtn.classList.contains('active')){
-        todoArr.forEach(({ value, active }, index) => {
-            if (active)
-                createList(value, active, index)
-        });
-    }
-}
+  setTimeout(() => {
+    alert.style.display = "none";
+  }, 5000);
+};
+
+const checkValidTask = (taskName) => {
+  if (taskName === "") {
+    showAlert("Enter any task name!");
+    return false;
+  }
+
+  if (todoArr.findIndex((obj) => obj.value === taskName) !== -1) {
+    showAlert("Same task is already  present!");
+    return false;
+  }
+
+  return true;
+};
 
 const addTask = () => {
-    const task = todoInput.value.trim();
-    todoInput.value = "";
-    if (task !== "") {
-        if (todoArr.findIndex(({ value, active }) => value === task) === -1) {
-            todoArr.unshift({
-                'value': task,
-                'active': true
-            });
-        }
-        else {
-            alert.style.display = 'block'
+  const task = todoInput.value.trim();
+  todoInput.value = "";
 
-            setTimeout(() => {
-                alert.style.display = 'none'
-            }, 5000)
-        }
-        showList();
-    }
+  if (checkValidTask(task)) {
+    todoArr.unshift({
+      value: task,
+      active: true,
+      dateTime: new Date(),
+    });
+  }
+  showList();
 };
 
 addBtn.addEventListener("click", () => {
-    addTask();
+  addTask();
 });
 
 todoInput.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-        addTask();
-    }
+  if (e.key === "Enter") {
+    addTask();
+  }
 });
 
-
 const deleteTask = (index) => {
-    todoArr.splice(index, 1);
-    showList();
+  todoArr.splice(index, 1);
+  showList();
 };
 
 const completeTask = (index) => {
-    todoArr[index].active = !todoArr[index].active;
+  todoArr[index].active = !todoArr[index].active;
+  showList();
+};
+
+const updateTask = (index, count, event) => {
+  const task = document.querySelectorAll(".task-name")[count];
+  const taskValue = task.textContent.trim();
+  if (event.key === "Enter") {
+    if (checkValidTask(taskValue)) {
+      todoArr[index].value = taskValue;
+      todoArr[index].dateTime = new Date();
+    }
+    task.setAttribute("contenteditable", "false");
     showList();
+  }
 };
 
-const editTask = (index) => {
-    const task = document.querySelector(`#task${index}`)
-    task.setAttribute("contenteditable" , "true");
-    task.addEventListener("keypress", (e) => {
-        if (e.key === "Enter") {
-            if(task.textContent!=="")
-                todoArr[index].value = task.textContent;
-            task.setAttribute("contenteditable" , "false")
-        }
-    });
+const editTask = (count) => {
+  const task = document.querySelectorAll(".task-name")[count];
+  task.setAttribute("contenteditable", "true");
+  task.style.outline = "1px solid blue";
 };
 
-const removeActiveClass = () => {
-    filterBtns.forEach(filterBtn => {
-        filterBtn.classList.remove('active');
-    });
-}
+const toggleActiveClass = (targetBtn) => {
+  filterBtns.forEach((filterBtn) => {
+    filterBtn.classList.remove("active");
+  });
+  targetBtn.classList.add("active");
+  showList();
+};
 
 allBtn.addEventListener("click", () => {
-    removeActiveClass();
-    allBtn.classList.add('active');
-    showList();
+  toggleActiveClass(allBtn);
 });
 
 activeBtn.addEventListener("click", () => {
-    removeActiveClass();
-    activeBtn.classList.add('active');
-    showList();
+  toggleActiveClass(activeBtn);
 });
 
 completeBtn.addEventListener("click", () => {
-    removeActiveClass();
-    completeBtn.classList.add('active');
-    showList();
+  toggleActiveClass(completeBtn);
 });
 
 clearBtn.addEventListener("click", () => {
-    todoArr = todoArr.filter(({ value, active }) => active === true);
-    removeActiveClass();
-    clearBtn.classList.add('active');
-    showList();
+  todoArr = todoArr.filter((obj) => obj.active === true);
+  toggleActiveClass(clearBtn);
 });
 
 showList();
